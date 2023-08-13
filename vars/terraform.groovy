4392ff3663598c921a0622e479caf3a2d293c953
Sreeni002 <sreenu.reddy002@gmail.com>
2023-08-13 09:14:50 +0530

    Make changes in groovy file

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,31 +1,32 @@
 def call() {
-    pipeline {
-        agent {
-            node {
-                label 'workstation'
-            }
-        }
-        parameters {
-            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pic environment')
-        }
-        stages {
-            stage('Terraform INIT') {
-                steps {
-                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
-                }
-            }
+  pipeline {
 
-            stage('Terraform Apply') {
-                steps {
-                    sh 'terraform apply -auto-approve -var-file=env-${env}/main.tfvars'
-                }
-            }
-        }
-
-        post {
-            always {
-                cleanWs()
-            }
-        }
+    agent {
+      node {
+        label 'workstation'
+      }
     }
+    parameters {
+      choice(name: 'env', choices: ['dev', 'prod'], description: 'Pic environment')
+    }
+
+    stages {
+
+      stage('Terraform INIT') {
+        steps {
+          sh 'terraform init -backend-config=env-${env}/state.tfvars'
+        }
+      }
+      stage('Terraform Apply') {
+        steps {
+          sh 'terraform apply -auto-approve -var-file=env-${env}/main.tfvars'
+        }
+      }
+    }
+    post {
+      always {
+        cleanWs()
+      }
+    }
+  }
 }
