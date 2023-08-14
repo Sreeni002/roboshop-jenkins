4a585e527047124bd7dc2ae2a27eaf97a680544d
Sreeni002 <sreenu.reddy002@gmail.com>
2023-08-14 22:14:30 +0530

    Push the code into NEXUS

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -16,7 +16,7 @@ def call() {
       stages {
         stage('Code Quality') {
           steps {
-            //sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.90.86:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+            sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.90.86:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
           }
         }
         stage('Run Unit Test cases') {
