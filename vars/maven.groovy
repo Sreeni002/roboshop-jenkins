4adc9379ad88281ca0898c39337ccf517d0a7acc
Sreeni002 <sreenu.reddy002@gmail.com>
2023-08-14 22:45:24 +0530

    Push the code into NEXUS

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -23,27 +23,40 @@ def call() {
           sh 'echo Code Quality'
         }
       }
-        stage('Run Unit Test cases') {
-          steps {
-            sh 'echo Run Unit Test cases'
-            // sh 'mvn test'
-          }
-        }
-        stage('CheckMarx SAST Scan') {
-          steps {
-            sh 'echo CheckMarx SAST Scan'
-          }
-        }
-        stage('CheckMarx SCA Scan') {
-          steps {
-            sh 'echo CheckMarx SCA Scan'
-          }
+      stage('Run Unit Test cases') {
+        steps {
+          sh 'echo Run Unit Test cases'
+          // sh 'mvn test'
         }
       }
-      post {
-        always {
-          cleanWs()
+      stage('CheckMarx SAST Scan') {
+        steps {
+          sh 'echo CheckMarx SAST Scan'
+        }
+      }
+      stage('CheckMarx SCA Scan') {
+        steps {
+          sh 'echo CheckMarx SCA Scan'
+        }
+      }
+      stage('Release Application') {
+        when {
+          expression {
+            env.TAG_NAME ==~ ".*"
+          }
+        }
+        steps {
+          sh 'mvn package ; cp target/${component}-1.0.jar ${component}.jar'
+          sh 'echo $TAG_NAME >VERSION'
+          sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
+          sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.81.83:8081/repository/${component}/${component}-${TAG_NAME}.zip'
         }
       }
     }
+    post {
+      always {
+        cleanWs()
+      }
+    }
   }
+}
--- vars/python.groovy
+++ vars/python.groovy
@@ -17,27 +17,39 @@ def call() {
           sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.90.86:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
         }
       }
-        stage('Run Unit Test cases') {
-          steps {
-            sh 'echo Run Unit Test cases'
-            //sh 'python3.6 -m unittest'
-          }
-        }
-        stage('CheckMarx SAST Scan') {
-          steps {
-            sh 'echo CheckMarx SAST Scan'
-          }
-        }
-        stage('CheckMarx SCA Scan') {
-          steps {
-            sh 'echo CheckMarx SCA Scan'
-          }
+      stage('Run Unit Test cases') {
+        steps {
+          sh 'echo Run Unit Test cases'
+          //sh 'python3.6 -m unittest'
         }
       }
-      post {
-        always {
-          cleanWs()
+      stage('CheckMarx SAST Scan') {
+        steps {
+          sh 'echo CheckMarx SAST Scan'
+        }
+      }
+      stage('CheckMarx SCA Scan') {
+        steps {
+          sh 'echo CheckMarx SCA Scan'
+        }
+      }
+      stage('Release Application') {
+        when {
+          expression {
+            env.TAG_NAME ==~ ".*"
+          }
+        }
+        steps {
+          sh 'echo $TAG_NAME >VERSION'
+          sh 'zip -r ${component}-${TAG_NAME}.zip *.ini *.py *.txt VERSION'
+          sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.81.83:8081/repository/${component}/${component}-${TAG_NAME}.zip'
         }
       }
     }
+    post {
+      always {
+        cleanWs()
+      }
+    }
   }
+}
